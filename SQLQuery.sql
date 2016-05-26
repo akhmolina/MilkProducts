421499d7eb00a586daf0cabd0500718a69505d3a
Valentina Akhmolina <valentina.ahmolina@yandex.ru>
2016-05-26 23:49:21 +0300

    Initial

---
+++ SQLQuery.sql
@@ -0,0 +1,14 @@
+SELECT CustomerId, RegistrationDateTime
+FROM Customers
+WHERE CustomerId IN 
+(
+SELECT DISTINCT CustomerId 
+FROM Purchaises 
+WHERE ProductName = 'молоко'
+AND CustomerId NOT IN
+(
+SELECT CustomerId 
+FROM Purchaises 
+WHERE ProductName = 'сметана' AND PurchaiseDatetime >= DATEADD(MONTH, -1, GETDATE())
+)
+)
